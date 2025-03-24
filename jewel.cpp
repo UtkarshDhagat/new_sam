d686ead23f1ef245cfea5d588bda45576eed7d20
UtkarshDhagat <122167569+UtkarshDhagat@users.noreply.github.com>
2025-03-25 02:38:09 +0530

    Update jewel.cpp

--- jewel.cpp
+++ jewel.cpp
@@ -94,3 +94,69 @@ int main() {
 
     return 0;
 }
+
+/*
+#include<iostream>
+using namespace std;
+const int PATH=3;
+int jewel=2;
+int wall=1;
+int open=0;
+int maxjewels=0;
+int mindepth=100000;
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+void bfs(int i,int j,int n,int jewels,int maze[50][50],int path[50][50],int bestpath[50][50],int depth){
+    if(i==n-1 && j==n-1){
+        if(jewels>maxjewels ||( jewels==maxjewels && depth<mindepth)){
+            maxjewels=jewels;
+            mindepth=depth;
+        for(int r=0;r<n;r++){
+            for (int c=0;c<n;c++){
+                bestpath[r][c]=path[r][c];
+            }
+        }
+        return;
+    }
+}
+    for(int d=0;d<4;d++){
+        int ni=i+dx[d];
+        int nj=j+dy[d];
+        if(ni>=0 && nj>=0 && ni<n && nj<n && maze[ni][nj]!=wall && path[ni][nj]==open){
+            path[ni][nj]=PATH;
+            int collect=0;
+            if (maze[ni][nj]==jewel){
+                    collect=1;
+            }
+            bfs(ni,nj,n,jewels+collect,maze,path,bestpath,depth+1);
+            path[ni][nj]=open;
+            }
+        }
+    }
+int main (){
+    int n;
+    cin>>n;
+    int i;
+    int j;
+    int maze[50][50];
+    int path[50][50];
+    int bestpath[50][50];
+    for (i=0;i<n;i++){
+        for (j=0;j<n;j++){
+            cin >>maze[i][j];
+        }
+    }
+    path[0][0]=PATH;
+    int startjewel=0;
+    if(maze[0][0]==jewel){
+        startjewel=1;
+    }
+    bfs(0, 0, n, startjewel, maze, path, bestpath, 0);
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            cout<<bestpath[i][j]<< " ";
+        }
+        cout<<endl;
+    }
+    cout<<maxjewels<<endl;
+}*/
