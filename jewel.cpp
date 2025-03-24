f849708daf42599d22dfe5ca4b32fbaeb3c28c82
UtkarshDhagat <122167569+UtkarshDhagat@users.noreply.github.com>
2025-03-24 21:49:21 +0530

    Update jewel.cpp

--- jewel.cpp
+++ jewel.cpp
@@ -1,84 +1,96 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-// Check if a move is valid
-bool isValid(int x, int y, int n, int maze[10][10], bool visited[10][10]) {
-    return (x >= 0 && y >= 0 && x < n && y < n && maze[x][y] != 1 && !visited[x][y]);
-}
+const int OPEN = 0;
+const int PATH = 3;
+const int WALL = 1;
+const int JEWEL = 2;
+const int MAX = 10;
 
-// DFS to find best path collecting max jewels
-void findPath(int x, int y, int n, int maze[10][10], bool visited[10][10],
-              int jewels, int &maxJewels, int path[10][10], int bestPath[10][10]) {
+int maxJewels = 0;
+int minDepth = INT_MAX;
 
-    if (x == n - 1 && y == n - 1) {  // reached exit
-        if (jewels > maxJewels) {   // found better path
+// Movement directions: right, down, left, up
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+// Recursive DFS function
+void dfs(int i, int j, int N, int maze[MAX][MAX], int path[MAX][MAX], int bestPath[MAX][MAX],
+         int jewels, int depth) {
+
+    // Base case: reached destination
+    if (i == N - 1 && j == N - 1) {
+        if (jewels > maxJewels || (jewels == maxJewels && depth < minDepth)) {
             maxJewels = jewels;
+            minDepth = depth;
 
-            // Save current path as best path
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    bestPath[i][j] = path[i][j];
+            // Save the best path
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    bestPath[r][c] = path[r][c];
+                }
+            }
         }
         return;
     }
 
-    // Move in 4 directions
-    int dx[] = {1, 0, -1, 0};  // down, right, up, left
-    int dy[] = {0, 1, 0, -1};
-
+    // Explore 4 directions
     for (int d = 0; d < 4; d++) {
-        int nx = x + dx[d];
-        int ny = y + dy[d];
+        int r = i + dx[d];
+        int c = j + dy[d];
 
-        if (isValid(nx, ny, n, maze, visited)) {
-            visited[nx][ny] = true;
-            path[nx][ny] = 3;  // mark path
-            int extra = 0;
-            if (maze[nx][ny] == 2) extra = 1;  // found jewel
+        // Check if move is valid
+        if (r >= 0 && r < N && c >= 0 && c < N && maze[r][c] != WALL && path[r][c] == OPEN) {
+            path[r][c] = PATH;
 
-            findPath(nx, ny, n, maze, visited, jewels + extra, maxJewels, path, bestPath);
+            int collect = 0;
+            if (maze[r][c] == JEWEL) {
+                collect = 1; // If this cell has a jewel, collect it
+            }
 
-            visited[nx][ny] = false;  // backtrack
-            path[nx][ny] = maze[nx][ny];  // restore cell
+            dfs(r, c, N, maze, path, bestPath, jewels + collect, depth + 1);
+
+            path[r][c] = OPEN; // Backtrack
         }
     }
 }
 
 int main() {
-    int t;
-    cin >> t;
+    int N;
+    cin >> N;
 
-    while (t--) {
-        int n;
-        cin >> n;
+    int maze[MAX][MAX];
+    int path[MAX][MAX] = {0};
+    int bestPath[MAX][MAX] = {0};
 
-        int maze[10][10], path[10][10], bestPath[10][10];
-        bool visited[10][10] = {false};
-        int maxJewels = 0;
-
-        // Read maze and prepare path arrays
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                cin >> maze[i][j];
-                path[i][j] = maze[i][j];
-                bestPath[i][j] = maze[i][j];
-            }
-
-        // Start from (0,0)
-        visited[0][0] = true;
-        path[0][0] = 3;
-        int startJewels = (maze[0][0] == 2) ? 1 : 0;
-
-        findPath(0, 0, n, maze, visited, startJewels, maxJewels, path, bestPath);
-
-        // Output path
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
-                cout << bestPath[i][j] << " ";
-            cout << endl;
+    // Input maze
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> maze[i][j];
         }
-        cout << maxJewels << endl << endl;
     }
+
+    // Start at (0,0)
+    path[0][0] = PATH;
+    int startJewels = 0;
+    if (maze[0][0] == JEWEL) {
+        startJewels = 1;
+    }
+
+    // Run DFS
+    dfs(0, 0, N, maze, path, bestPath, startJewels, 0);
+
+    // Print best path
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << bestPath[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Print total jewels collected
+    cout << maxJewels << endl;
 
     return 0;
 }
